5346317c9ee5902c9d053f8afc45de99f8be9045
devCordain <pctkar@gmail.com>
2020-03-29 18:45:43 +0200

    Initial commit

---
+++ Webshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
