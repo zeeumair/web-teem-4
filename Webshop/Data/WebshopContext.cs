1c6efd73c8d6ac28377d1c0c2eef709d2352908f
devCordain <pctkar@gmail.com>
2020-04-13 12:49:42 +0200

    Added Orders class

--- Webshop/Data/WebshopContext.cs
+++ Webshop/Data/WebshopContext.cs
@@ -11,5 +11,7 @@ namespace Webshop.Data
         }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ Webshop/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webshop.Models
+{
+    public class Order
+    {
+        public virtual User User { get; set; }
+
+        public string PaymentOption { get; set; }
+        public Double TotalAmount { get; set; }
+        public string DeliveryOption { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
