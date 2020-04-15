6f1bd3c2a8e8f55905f2fafa51ef9ffb54a27daf
devCordain <pctkar@gmail.com>
2020-04-15 08:38:21 +0200

    WEB4-37 Created products views, removed index and home controller

--- Webshop/Startup.cs
+++ Webshop/Startup.cs
@@ -54,7 +54,7 @@ namespace Webshop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
