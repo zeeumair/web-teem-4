439898ef59260dea946d6505d17a63111b78af57
Andreas <Andreas_Zachrisson@hotmail.com>
2020-05-07 12:20:36 +0200

    Added migration for a column in Currencies, for LastUpdated.

--- Webshop/Migrations/20200507101818_CurrencyLastUpdatedColumn.Designer.cs
+++ Webshop/Migrations/20200507101818_CurrencyLastUpdatedColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webshop.Models;
 
 namespace Webshop.Migrations
 {
     [DbContext(typeof(IdentityAppContext))]
-    partial class WebshopContextModelSnapshot : ModelSnapshot
+    [Migration("20200507101818_CurrencyLastUpdatedColumn")]
+    partial class CurrencyLastUpdatedColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
