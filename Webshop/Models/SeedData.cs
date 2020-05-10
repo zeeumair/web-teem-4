d6957db2b4a5992e98819e29152fc801e7ee55a0
Andreas <Andreas_Zachrisson@hotmail.com>
2020-05-10 10:42:31 +0200

    Implemented UserManager for seeding a user so that you can login or logout with the seeded data.

--- Webshop/Models/SeedData.cs
+++ Webshop/Models/SeedData.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Webshop.Models;
 using System.IO;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace Webshop
 {
@@ -37,90 +38,91 @@ namespace Webshop
 
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            using (var context = new IdentityAppContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<IdentityAppContext>>()))
+            var context = new IdentityAppContext(serviceProvider.GetRequiredService<DbContextOptions<IdentityAppContext>>());
+            context.Database.EnsureDeleted();
+            context.Database.Migrate();
+
+            var user = new User
             {
-                context.Database.EnsureDeleted();
-                context.Database.Migrate();
-
-                var user = new User
+                FirstName = "Test",
+                LastName = "User",
+                Password = "!1Aaaa",
+                StreetAdress = "Gogubbegatan 3",
+                PostNumber = "41706",
+                City = "Gothenburg",
+                Country = "Sweden",
+                Email = "omgzshoezz@gmail.com",
+                Currency = "SEK",
+                PhoneNumber = "0700000000",
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = "omgzshoezz@gmail.com"
+            };
+            var passwordHasher = new PasswordHasher<User>();
+            user.PasswordHash = passwordHasher.HashPassword(user, "!1Aaaa");
+            var manager = serviceProvider.GetRequiredService<UserManager<User>>();
+            manager.CreateAsync(user).Wait();
+            
+            var products = new List<Product> { 
+                new Product
                 {
-                    FirstName = "Test",
-                    LastName = "User",
-                    Password = "password",
-                    StreetAdress = "Gogubbegatan 3",
-                    PostNumber = "41706",
-                    City = "Gothenburg",
-                    Country = "Sweden",
-                    Email = "omgzshoezz@gmail.com",
-                    Currency = "SEK",
-                    PhoneNumber = "0700000000"
-                };
-
-                var products = new List<Product> { 
-                    new Product
-                    {
-                        Name = "Air Jordans",
-                        Price = 100,
-                        Image = ReadFile("Images/airJordans.jpg"),
-                        Description = "Fly high like Michael",
-                        Category = "sport",
-                        CreatedAt = DateTime.Today
-                    },
-                    new Product
-                    {
-                        Name = "Nike Air Zoom",
-                        Price = 100,
-                        Image = ReadFile("Images/NikeAirZoom.jpg"),
-                        Description = "Do something like someone",
-                        Category = "sport",
-                        CreatedAt = DateTime.Today
-                    },
-                    new Product
-                    {
-                        Name = "Nike Mercurial Vapor",
-                        Price = 100,
-                        Image = ReadFile("Images/NikeMercurialVapor.jpg"),
-                        Description = "Play Ball like Messi",
-                        Category = "sport",
-                        CreatedAt = DateTime.Today
-                    } 
-                };
-
-                var order = new Order
+                    Name = "Air Jordans",
+                    Price = 100,
+                    Image = ReadFile("Images/airJordans.jpg"),
+                    Description = "Fly high like Michael",
+                    Category = "sport",
+                    CreatedAt = DateTime.Today
+                },
+                new Product
                 {
-                    User = context.Users.Find(1),
-                    PaymentOption = "Swish",
-                    TotalAmount = 300,
-                    DeliveryOption = "2-5 days"
-                };
-
-                var orderItems = new List<OrderItem>();
-
-                products.ForEach(product => {
-                    orderItems.Add(
-                        new OrderItem
-                        {
-                            Order = order,
-                            Product = product,
-                            Quantity = 1
-                        });
-                });
-                context.OrderItems.AddRange(orderItems);
-
-                var currencyRates = CurrencyManager.GetCurrencyRates().Result;
-                foreach (KeyValuePair<string, double> item in  currencyRates.Rates)
+                    Name = "Nike Air Zoom",
+                    Price = 100,
+                    Image = ReadFile("Images/NikeAirZoom.jpg"),
+                    Description = "Do something like someone",
+                    Category = "sport",
+                    CreatedAt = DateTime.Today
+                },
+                new Product
                 {
-                    context.Currencies.Add(new Currency
+                    Name = "Nike Mercurial Vapor",
+                    Price = 100,
+                    Image = ReadFile("Images/NikeMercurialVapor.jpg"),
+                    Description = "Play Ball like Messi",
+                    Category = "sport",
+                    CreatedAt = DateTime.Today
+                } 
+            };
+
+            var order = new Order
+            {
+                User = context.Users.FirstOrDefault(),
+                PaymentOption = "Swish",
+                TotalAmount = 300,
+                DeliveryOption = "2-5 days"
+            };
+
+            var orderItems = new List<OrderItem>();
+            products.ForEach(product => {
+                orderItems.Add(
+                    new OrderItem
                     {
-                        CurrencyCode = item.Key,
-                        CurrencyRate = item.Value
+                        Order = order,
+                        Product = product,
+                        Quantity = 1
                     });
-                }
+            });
+            context.OrderItems.AddRange(orderItems);
 
-                context.SaveChanges();
+            var currencyRates = CurrencyManager.GetCurrencyRates().Result;
+            foreach (KeyValuePair<string, double> item in  currencyRates.Rates)
+            {
+                context.Currencies.Add(new Currency
+                {
+                    CurrencyCode = item.Key,
+                    CurrencyRate = item.Value
+                });
             }
+            context.SaveChanges();
+            context.Dispose();
         }
     }
 }
