71d3f3639606fbb30aba33f92b25a33c2d82743e
devCordain <pctkar@gmail.com>
2020-05-17 13:06:57 +0200

    WEB4-136 Added complimentary chuck norris fact

---
+++ Webshop/wwwroot/js/OrderJoke.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    this.GetRandomJoke();
+}
+function GetRandomJoke() {
+    var url = 'https://api.chucknorris.io/jokes/random';
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function () {
+        var data = JSON.parse(request.responseText);
+        renderHTML(data);
+    };
+    request.send();
+}
+
+function renderHTML(data) {
+    var jokeContainer = document.getElementById("jokeContainer");
+    var htmlString = "<p>" + data.value + "</p>";
+    jokeContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
