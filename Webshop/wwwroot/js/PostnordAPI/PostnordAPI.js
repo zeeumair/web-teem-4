0ef54f64381093e14efc7d98e21ab463fe6b3391
devCordain <pctkar@gmail.com>
2020-05-17 13:11:31 +0200

    WEB4-28 Still getting error 'No Access-Control-Allow-Origin'

--- Webshop/wwwroot/js/PostnordAPI/PostnordAPI.js
+++ Webshop/wwwroot/js/PostnordAPI/PostnordAPI.js
@@ -1,33 +1,19 @@
-﻿GetServiceCodes();
-
+﻿window.onload = function () {
+    this.GetServiceCodes();
+}
 function GetServiceCodes() {
-
     var apikey = '87a7d90ab3e0cb85d3a18ab17afd31d6';
-    //var country = document.getElementById("Country").value;
-    var baseUrl = 'https://atapi2.postnord.com/test?';
+    var url = 'https://atapi2.postnord.com/rest/shipment/v3/edi/servicecodes?apikey=';
     var request = new XMLHttpRequest();
-
-    request.open('GET', baseUrl + 'apikey=' + apikey);
+    request.open('GET', url + apikey);
     request.onload = function () {
-        console.log(request.responseText);
+        var data = JSON.parse(request.responseText);
+        console.log(data);
     };
     request.send();
+}
 
-    //$.ajax({
-    //    url: 'https://atapi2.postnord.com/rest/shipment/v3/edi/servicecodes?apikey=' + apikey,
-    //    dataType: 'json',
-    //    contentType: 'application/json',
-    //    success: function (data) {
-    //        var html = '';
-    //        var serviceArray = data.map(function (item) {
-    //            return { value: item.serviceCodeDetails.ServiceName, name: item.serviceCodeDetails.ServiceName};
-    //        });
-    //        serviceArray.each(serviceArray.value,
-    //            html = html + '<option value="' + value + '">' + data.issuerCountry + '</option>' + '');
-    //        document.querySelector('#deliveryTime').insertAdjacentHTML('afterbegin', html);
-    //    }, 
-    //    error: function() {
-    //        document.getElementById("Country").value = "No available mailing services in your country"
-    //    }
-    //});
+function renderHTML(data) {
+
+
 }
