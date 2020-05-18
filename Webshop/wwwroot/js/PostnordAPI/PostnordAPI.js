ade5a0338f76259ef2d2576e8255027c88e8670a
devCordain <pctkar@gmail.com>
2020-05-18 11:36:15 +0200

    WEB4-28 test with proxy

--- Webshop/wwwroot/js/PostnordAPI/PostnordAPI.js
+++ Webshop/wwwroot/js/PostnordAPI/PostnordAPI.js
@@ -3,10 +3,9 @@
 }
 function GetServiceCodes() {
     var apikey = '87a7d90ab3e0cb85d3a18ab17afd31d6';
-    var url = 'https://atapi2.postnord.com/rest/shipment/v3/edi/servicecodes?apikey=';
+    var url = 'https://cors-anywhere.herokuapp.com/https://atapi2.postnord.com/rest/shipment/v3/edi/servicecodes?apikey=';
     var request = new XMLHttpRequest();
     request.open('GET', url + apikey);
-    request.withCredentials = true;
     request.onload = function () {
         var data = JSON.parse(request.responseText);
         console.log(data);
